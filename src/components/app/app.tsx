890e7c517254299047bbf1e146aa5eb2627806a4
Pikul Maria <mariapikul20002@gmail.com>
2023-10-08 23:50:27 +0300

    3.13. Маршрут по тайным тропам

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,4 +1,12 @@
-import MainPage from "../main-page/main-page";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import MainPage from '../main-page/main-page';
+import SignIn from '../sing-in/sing-in';
+import PrivateRoute from '../private-route/private-route';
+import MyList from '../my-list/my-list';
+import MoviePage from '../movie-page/movie-page';
+import MoviePageReviews from '../movie-page-reviews/movie-page-reviews';
+import Player from '../player/player';
+import NotFoundPage from '../not-found/not-found';
 
 interface AppProps {
   mainPageData: {
@@ -11,12 +19,28 @@ interface AppProps {
 const App: React.FC<AppProps> = (props) => {
   const { mainPageData } = props;
 
+  
   return (
-    <MainPage
-      title={mainPageData.title}
-      genre={mainPageData.genre}
-      releaseDate={mainPageData.releaseDate}
-    />
+    <Router>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <MainPage
+              title={mainPageData.title}
+              genre={mainPageData.genre}
+              releaseDate={mainPageData.releaseDate}
+            />
+          }
+        />
+        <Route path="/login" element={<SignIn />} />
+        <Route path="/mylist" element={<PrivateRoute component={<MyList />} />} />
+        <Route path="/films/:id" element={<MoviePage />} />
+        <Route path="/films/:id/review" element={<MoviePageReviews />} />
+        <Route path="/player/:id" element={<Player />} />
+        <Route path="*" element={<NotFoundPage />} /> 
+      </Routes>
+    </Router>
   );
 };
 
---
+++ src/components/my-list/my-list.tsx
@@ -0,0 +1,9 @@
+const MyList = () => {
+  return (
+    <div>
+      {/* Тут будет контент для страницы MyList */}
+    </div>
+  );
+};
+
+export default MyList;
